624fcfb679cf46b399c1b6d09ee1837918a741ba
pavel.genuine <pavel.play@gmail.com>
2022-07-21 16:21:19 +0600

    lan section

--- src/Components/Languages/Languages.js
+++ src/Components/Languages/Languages.js
@@ -3,7 +3,16 @@ import React from 'react';
 const Languages = () => {
     return (
         <div className='pt-[10%] h-[100vh] bg-[#171B26]'>
-         
+          <div className='w-[85%]'>
+          <div className='flex '>
+                <div className='flex justify-end'>
+                <h1 className='mt-10 ml-60 text-[white] text-6xl font-semibold'>
+                    All Your Favorite
+                    Stacks Under One Hood
+                </h1>
+                </div>
+               
+          </div>
 
         </div>
     );
