90b653137d084a27e7b1437f7ecb6b82eda34dfd
pavel.genuine <pavel.play@gmail.com>
2022-07-21 16:28:30 +0600

    video section completed

--- src/Components/CodeVideo/Codevideo.js
+++ src/Components/CodeVideo/Codevideo.js
@@ -11,7 +11,10 @@ const Codevideo = () => {
                         <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
 
                             <p className='text-slate-400 my-4'> coderAccess is simplifing your DevDream...</p>
- </h2>
+
+                            <span class="block text-[white]">Ready to dive in?</span>
+                            <span class="block text-indigo-600">Start exploring today.</span>
+                        </h2>
                        
                            <button className='btn btn-sm  glass  my-5'>Get Started</button>
                        
