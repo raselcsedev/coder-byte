9bd553683eee035f8aed6663feecc614f89523c4
pavel.genuine <pavel.play@gmail.com>
2022-07-21 16:26:36 +0600

    video added

--- src/Components/CodeVideo/Codevideo.js
+++ src/Components/CodeVideo/Codevideo.js
@@ -4,7 +4,17 @@ import video from '../../code-video.mp4'
 const Codevideo = () => {
     return (
         <div className='bg-[#171B26] h-[120vh] pt-[5%]'>
-            
+            <div className='w-[60%] mx-auto'>
+               
+                <div>
+                    <div >
+                       
+                        
+                    </div>
+                </div>
+
+                <video className='rounded-xl border-2 border-slate-400 ' src={video} autoPlay loop muted></video>
+            </div>
         </div>
     );
 };
