e2ae6315236c70e06a21f2c6fb1d0a015e3a5563
pavel.genuine <pavel.play@gmail.com>
2022-07-21 16:24:21 +0600

    featured companies

--- src/Components/FeaturedCompanies/FeaturedCompanies.js
+++ src/Components/FeaturedCompanies/FeaturedCompanies.js
@@ -4,7 +4,6 @@ const FeaturedCompanies = () => {
     return (
         <div className='bg-[#171B26] h-[100vh] pt-[10%]'>
 
-            <img className='w-[80%] mx-auto' src="https://i.ibb.co/kDHsQxr/scrnli-7-21-2022-12-04-49-PM.png" alt="" />
 
         </div>
     );
