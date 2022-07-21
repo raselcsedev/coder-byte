d05117963a1ab8335faa3e6ff47619bcde09e54f
pavel.genuine <pavel.play@gmail.com>
2022-07-21 16:19:30 +0600

    language options

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import SignIn from './Components/Auth/SignIn';
 import SignUp from './Components/Auth/SignUp';
+import Codevideo from './Components/CodeVideo/Codevideo';
+import FeaturedCompanies from './Components/FeaturedCompanies/FeaturedCompanies';
 // import logo from './logo.svg';
 // import { Counter } from './features/counter/Counter';
 import Header from './Components/Header/Header';
+import Languages from './Components/Languages/Languages';
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
 
       <Routes>
      
+        <Route path='/lan' element={<Languages></Languages>}></Route>
+        <Route path='/video' element={<Codevideo></Codevideo>}></Route>
+        <Route path='/com' element={<FeaturedCompanies></FeaturedCompanies>}></Route>
         <Route path='/sign-up' element={<SignUp></SignUp>}></Route>
         <Route path='/sign-in' element={<SignIn></SignIn>}></Route>
 
---
+++ src/Components/Languages/Languages.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Languages = () => {
+    return (
+        <div className='pt-[10%] h-[100vh] bg-[#171B26]'>
+         
+
+        </div>
+    );
+};
+
+export default Languages;
