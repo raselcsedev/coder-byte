4b958aaa816e8b1d527db3fbe1b11e4bcbdd34c4
Fahim Rimon <fahimrimon21@gmail.com>
2022-07-28 11:55:23 +0600

    check

--- src/Components/Courses/Course.js
+++ src/Components/Courses/Course.js
@@ -1,39 +1,36 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useEffect, useState } from "react";
 
 const Course = () => {
-    const [datas, setdata] = useState([]);
+  const [datas, setdata] = useState([]);
 
-    useEffect(() => {
-        fetch('Data.json')
-            .then(res => res.json())
-            .then(data => setdata(data))
-
-    }, [])
-    const data = datas.slice(0, 9)
-    return (
-        <div className=''>
-                <h2 className='text-4xl font-smibold font-serif text-center py-12'>Sharpen Your Skills With Our Courses</h2>
-            <section className=' flex justify-center mb-36 '>
-                <div className=' grid grid-cols-1 lg:grid-cols-3  gap-12'>
-                    {data.map(data => <>
-
-
-                        <div class="card w-[100%] md:w-96  bg-base-100 shadow-2xl">
-                            <div class="card-body  ">
-                                <div className='flex'>
-                                    <img width='50px' height='50px' src={data.img} alt="" />
-                                    <h2 class=" ml-4 card-title">{data.title}</h2>
-                                </div>
-
-                            </div>
-                        </div>
-                    </>)}
+  useEffect(() => {
+    fetch("Data.json")
+      .then((res) => res.json())
+      .then((data) => setdata(data));
+  }, []);
+  const data = datas.slice(0, 9);
+  return (
+    <div className="">
+      <h2 className="text-4xl font-smibold font-serif text-center py-12">Sharpen Your Skills With Our</h2>
+      <section className=" flex justify-center mb-36 ">
+        <div className=" grid grid-cols-1 lg:grid-cols-3  gap-12">
+          {data.map((data) => (
+            <>
+              <div class="card w-[100%] md:w-96  bg-base-100 shadow-2xl">
+                <div class="card-body  ">
+                  <div className="flex">
+                    <img width="50px" height="50px" src={data.img} alt="" />
+                    <h2 class=" ml-4 card-title">{data.title}</h2>
+                  </div>
                 </div>
-            </section>
-
+              </div>
+            </>
+          ))}
         </div>
-    );
+      </section>
+    </div>
+  );
 };
 
 export default Course;
