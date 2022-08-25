73722e5dda8285bd82009d38117e1d4881aba53d
pavel.genuine <pavel.play@gmail.com>
2022-08-25 22:13:18 +0600

    audio listen btn

--- src/Components/Blogs/BlogDetail.js
+++ src/Components/Blogs/BlogDetail.js
@@ -172,15 +172,17 @@ The versions must be compatible, so if a peerDependency is listed as 2.x, you ca
                 <div className="my-4 ">
 
                     {
-                        listen && <button className="btn btn-xs bg-[green] hover:bg-[brown]  pb-7 flex  text-white" onClick={() => { handlePause(); pauseHandler() }} >Pause <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mt-1 text-white h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M10 9v6m4-6v6m7-3a9 9 0 11-18 0 9 9 0 0118 0z" />
-                        </svg></button>}
-                    {
-                        pause && <button onClick={() => { handleListen(); handlePlay() }} className="btn btn-xs bg-[green] hover:bg-[brown] pb-7 flex text-white">
-                            Listen<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mt-1 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        listen && <button onClick={() => { handlePause(); pauseHandler() }} className="border-2 bg-black border-black hover:bg-slate-700 rounded-full w-28 h-10 flex items-center  space-x-1 py-3 pl-3 pr-2 text-white"> <span className='font-semibold '>Paush</span>
+
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-24 w-24" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M10 9v6m4-6v6m7-3a9 9 0 11-18 0 9 9 0 0118 0z" />
                             </svg>
+                        </button>
+                    }
+                    {
+                        pause && <button onClick={() => { handleListen(); handlePlay() }} className="border-2 border-black bg-black hover:bg-slate-700 rounded-full w-28 h-10 flex items-center  px-3 space-x-2 py-3 text-white"> <span className='font-semibold '>Listen</span>
+
+                            <img className='w-8' src="https://i.ibb.co/vVVrLcZ/listen-on-google-podcasts-icon.png" alt="" />
                         </button>
                     }
 
